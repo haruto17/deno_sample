2b3fb67f3c49bc83eea9fc9979f4d09e74bf7619
haruto17 <s1902272@sendai-nct.jp>
2022-06-14 23:02:37 +0900

    ルールの表記

--- server.ts
+++ server.ts
@@ -2,8 +2,7 @@ import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 import { Util } from "./util.ts";
 
-const defaultWord = "しりとり";
-let previousWord = defaultWord;
+let previousWord = "しりとり";
 let stockWord = ["しりとり"];
 
 console.log("Listening on http://localhost:8000");
